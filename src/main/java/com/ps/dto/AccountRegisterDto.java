27d996f6d80230ae5c21160b3588dcc3acb724e0
sam <spike19820318@gmail.com>
2017-05-05 11:34:40 +0800

    錯誤訊息改為中文顯示

--- src/main/java/com/ps/dto/AccountRegisterDto.java
+++ src/main/java/com/ps/dto/AccountRegisterDto.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
@@ -14,24 +15,25 @@ import java.util.List;
  * Created by samchu on 2017/2/15.
  */
 @Data
-@ApiModel(description = "帳號註冊資料")
+@ApiModel(description = "帐号注册资料")
 public class AccountRegisterDto {
-    @ApiModelProperty(value = "帳號", required = true, example = "sam.chu")
+    @ApiModelProperty(value = "帐号", required = true, example = "sam.chu")
     @NotNull
-    @Size(min = 5, max = 30)
+    @Length(min = 5, max = 30, message = "密码长度 需介于 {min} 到 {max} 之间")
     private String username;
     @ApiModelProperty(value = "信箱", required = true, example = "sam@gmail.com")
     @NotNull
-    @Email
-    @Size(min = 5, max = 255)
+    @Email(message = "信箱格式不正确")
+    @Length(min = 1, max = 255, message = "信箱长度 需介于 {min} 到 {max} 之间")
     private String email;
-    @ApiModelProperty(value = "密碼", required = true, example = "12345678")
+    @ApiModelProperty(value = "密码", required = true, example = "12345678")
     @NotNull
-    @Size(min = 8, max = 255)
+    @Length(min = 8, max = 255, message = "密码长度 需介于 {min} 到 {max} 之间")
     private String password;
 
-    @ApiModelProperty(value = "角色代碼 ROLE_USER", required = true, dataType = "Array[string]")
+    @ApiModelProperty(value = "角色代码 ROLE_USER", required = true, dataType = "Array[string]")
     @NotNull
     @NotEmpty
+    @Size(min = 1, message = "至少必须勾选一角色")
     private List<String> roles;
 }
